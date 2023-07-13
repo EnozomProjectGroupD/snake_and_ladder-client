19259cb0a2e927f0b2ef5089956a59bbd546cf22
Mohamed-Elemam <mohamed.elemam.younis@gmail.com>
2023-07-13 12:53:24 +0300

    fixed issue

--- src/components/playersInRoom.jsx
+++ src/components/playersInRoom.jsx
@@ -1,31 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { authToken } from './Startgame';
 import axios from 'axios';
 
-export default function PlayersInRoom({theId} ) {
+export default function PlayersInRoom({ theId }) {
   const [apiData, setApiData] = useState(null);
 
   async function getGame(id) {
     try {
-      const { data } = await axios.get(`http://localhost:3000/api/game/get/6`, {
+      const { data } = await axios.get(`http://localhost:3000/api/game/get/${id}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('userToken')}`,
         },
       });
       setApiData(data.game);
-      console.log(data.game)
     } catch (error) {
       console.log(error);
     }
   }
-  useEffect((theId) => {
 
+  useEffect(() => {
     getGame(theId);
   }, [theId]);
 
   return (
     <>
-      
+      {apiData && (
         <div className="text-center mt-5">
           <table className="table my-3">
             <thead>
@@ -44,7 +42,7 @@ export default function PlayersInRoom({theId} ) {
             </tbody>
           </table>
         </div>
-      
+      )}
     </>
   );
 }
